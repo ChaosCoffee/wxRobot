3e46a24eeae99c85fbe927edb9e1ab7810b52df4
chaoscoffee <1104855822@qq.com>
2018-07-30 14:25:25 +0800

    chat group push message

--- api/news.py
+++ api/news.py
@@ -10,7 +10,7 @@ from . import wxapi
 hour = 12
 minute = 30
 execFlag = True
-names = ['Test','兄弟姐弟'] 
+names = ['Test','测试'] 
 execTime = "08:30"
 
 class myThread (threading.Thread):
@@ -47,7 +47,8 @@ def send_news():
     wxapi.logger.info("newsAPI- 当前时间是 %s" % i)
     try:
         contents = get_news()
-        send_chatrooms_msg(names,'[每日一句又来啦~]\n%s\n%s' % (contents[0],contents[1]))
+        #多群组则循环调用发送接口
+        send_single_chatroom_msg(names[1],'[每日一句又来啦~]\n%s\n%s' % (contents[0],contents[1]),'每日一句')
     except:
         wxapi.logger.error("当天[每日一句]任务执行失败...")
 
@@ -56,7 +57,7 @@ def schedule_weather():
     wxapi.logger.info("WeatherAPI- 当前时间是 %s" % i)
     try:
         contents = wxapi.query_weather('上海')
-        send_chatrooms_msg(names,contents)
+        send_single_chatroom_msg(names[1],contents,'天气预报')
     except:
         wxapi.logger.error("当天[天气预报]任务执行失败...")
 
@@ -66,8 +67,20 @@ def schedule_news():
     t = Timer(30, send_news)
     t.start()
 
-def send_chatrooms_msg(senders,context):
-    sender = senders[1]
+#暂不支持多个群发送
+def send_chatrooms_msg(senders,context,taskName):
+    sender = senders[1] 
+    itchat.get_chatrooms(update = True)
+    iRoom = itchat.search_chatrooms(name = sender)
+    for room in iRoom:
+        if room['NickName'] == sender:
+            userName = room['UserName']
+            break
+    time.sleep(random.randint(5,12))
+    itchat.send_msg(context, userName)
+    wxapi.logger.info("定时任务[%s],发送时间：[%s], 发送到: [%s], 发送内容：[%s]" ,taskName,datetime.datetime.now(), sender ,context)
+
+def send_single_chatroom_msg(sender,context,taskName):
     itchat.get_chatrooms(update = True)
     iRoom = itchat.search_chatrooms(name = sender)
     #wxapi.logger.info(iRoom)
@@ -77,7 +90,7 @@ def send_chatrooms_msg(senders,context):
             break
     time.sleep(random.randint(5,12))
     itchat.send_msg(context, userName)
-    wxapi.logger.info("定时任务[%s],发送时间：[%s], 发送到: [%s], 发送内容：[%s]" ,'每日一句',datetime.datetime.now(), sender ,context)
+    wxapi.logger.info("定时任务[%s],发送时间：[%s], 发送到: [%s], 发送内容：[%s]" ,taskName,datetime.datetime.now(), sender ,context)
 
 def run():
     schedule.every().day.at(execTime).do(send_news) #规定每天08：30执行job()函数
